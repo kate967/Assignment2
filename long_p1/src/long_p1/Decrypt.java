bef982248f352cb860d793e21f7ddd51ea9e4524
kate967 <46581081+kate967@users.noreply.github.com>
2019-09-20 17:34:58 -0400

    adding problem 1

--- long_p1/src/long_p1/Decrypt.java
+++ long_p1/src/long_p1/Decrypt.java
@@ -2,7 +2,7 @@ package long_p1;
 
 import java.util.Scanner;
 
-public class Encrypt {
+public class Decrypt {
 
 	public static void main(String[] args) {
 		Scanner scnr = new Scanner(System.in);
@@ -10,38 +10,41 @@ public class Encrypt {
 		int tempInt = 0;
 		int[] array = new int[4];
 		
-		System.out.println("Enter a 4 digit number: ");
+		System.out.println("Enter your encrypted number: ");
 		userInput = scnr.nextInt();
 		
-		//puts userInput into an array
 		for(i = 3; i >= 0; i--)
 		{
 			array[i] = userInput%10;
 			userInput /= 10;
 		}
 		
-		for(i = 0; i < 4; i++)
-		{
-			array[i] += 7;
-			array[i] = array[i]%10;
-		}
-		
-		//switching first and third
 		tempInt = array[0];
 		array[0] = array[2];
 		array[2] = tempInt;
-		//switching second and fourth
 		tempInt = array[1];
 		array[1] = array[3];
 		array[3] = tempInt;
 		tempInt = 0;
 		
-		System.out.println("Your encrypted number is: ");
+		for(i = 0; i < 4; i++)
+		{
+			if(array[i] + 10 > 16)
+			{
+				array[i] -= 7;
+			}
+			else
+			{
+				array[i] +=10;
+				array[i] -= 7;
+			}
+		}
+		
+		System.out.println("Your decrypted number is: ");
 		
 		for(i = 0; i < 4; i++)
 		{
 			System.out.print(array[i]);
 		}
-		
-	}	
+	}
 }
