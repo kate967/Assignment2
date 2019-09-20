f6a80751603ebe166b0c5fe50ccd179b21e65568
kate967 <46581081+kate967@users.noreply.github.com>
2019-09-20 16:08:37 -0400

    My first commit

---
+++ long_p1/src/long_p1/Encrypt.java
@@ -0,0 +1,17 @@
+package long_p1;
+
+import java.util.Scanner;
+
+public class Encrypt {
+
+	public static void main(String[] args) {
+		Scanner scnr = new Scanner(System.in);
+		int userInput;
+		
+		System.out.println("Enter a 4 digit number: ");
+		userInput = scnr.nextInt();
+		System.out.println("You entered: " + userInput);
+
+	}
+
+}
